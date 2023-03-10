1d7c240be368c5e506221652b53a8e06e406da5b
AdamStar <adam.starostik@icloud.com>
2023-03-10 13:38:03 +0100

    Day 37: Code is ready

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import os
 import cv2
 import time
+from emailing import send_email
+import glob
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
+
+
+def clean_folder():
+    print("clean_folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean_folder function ended")
+
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -20,13 +36,34 @@ while True:
     dil_frame = cv2.dilate(thrash_frame, None, iterations=2)
     cv2.imshow("My video", dil_frame)
 
-    contours , check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h, = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any:
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        emai_thread = Thread(target=send_email, args=(image_with_object, ))
+        emai_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        emai_thread.start()
+
+
+    print(status_list)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -35,3 +72,5 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
